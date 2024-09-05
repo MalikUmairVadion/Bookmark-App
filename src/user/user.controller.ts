583dcc4771f106d6643e1b21807cc8a971d37b8b
MalikUmairVadion <malik.umair@softrove.com>
2024-09-05 18:48:34 +0500

    Edit User API

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,31 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  Put,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '@prisma/client';
 import { Request } from 'express';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private usersService: UserService) {}
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
+  }
+
+  @Put()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.usersService.editUser(userId, dto);
   }
 }
